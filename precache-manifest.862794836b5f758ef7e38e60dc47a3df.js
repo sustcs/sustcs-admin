7ebd395b6803e20065a98965469f90e45bb75e56
makergyt <me@makergyt.com>
2019-04-30 09:30:51 +0800

    Updates

---
+++ precache-manifest.862794836b5f758ef7e38e60dc47a3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5e7fd86f53af49477c997f79db36fe",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "6d8eaa8589afc1cc616a",
+    "url": "/admin/static/css/2.864cb12e.chunk.css"
+  },
+  {
+    "revision": "3ef11c0b5dab9ab1b0f6",
+    "url": "/admin/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "6d8eaa8589afc1cc616a",
+    "url": "/admin/static/js/2.3fc6fc61.chunk.js"
+  },
+  {
+    "revision": "3ef11c0b5dab9ab1b0f6",
+    "url": "/admin/static/js/main.28e90ea4.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
