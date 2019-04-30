669f733c38cadf8afa79cd4dbf2990abb05e0bf4
makergyt <me@makergyt.com>
2019-04-30 09:46:56 +0800

    rebuild github

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      echo: ''
-    };
-  }
-  componentDidMount() {
-    fetch('/')
-      .then(res => {
-        console.log(res);
-        if (res.ok) {
-          res.text().then(data => {
-            this.setState({
-              echo: data
-            });
-          });
-        }
-      })
-      .catch(res => {
-        console.log(res.status);
-      });
-  }
-  componentWillUnmount() {
-    this.serverRequest.abort();
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     echo: ''
+  //   };
+  // }
+  // componentDidMount() {
+  //   fetch('/')
+  //     .then(res => {
+  //       console.log(res);
+  //       if (res.ok) {
+  //         res.text().then(data => {
+  //           this.setState({
+  //             echo: data
+  //           });
+  //         });
+  //       }
+  //     })
+  //     .catch(res => {
+  //       console.log(res.status);
+  //     });
+  // }
+  // componentWillUnmount() {
+  //   this.serverRequest.abort();
+  // }
   render() {
     return (
       <div className="APP">
-        <Button type="primary">{this.state.echo}</Button>
+        <Button type="primary">hello react</Button>
       </div>
     );
   }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,4 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
-  app.use(proxy('/', { target: 'http://127.0.0.1:7001' }));
+  // app.use(proxy('/', { target: 'http://127.0.0.1:7001' }));
 };
